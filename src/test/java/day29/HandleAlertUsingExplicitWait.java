3218b3df328c51829db284036338295e40e9697f
aimeerim1 <aiimuratbekovaa@gmail.com>
2025-08-16 19:46:22 +0600

    handle frames

--- src/test/java/day29/HandleAlertUsingExplicitWait.java
+++ src/test/java/day29/HandleAlertUsingExplicitWait.java
@@ -15,15 +15,11 @@ public class HandleAlertUsingExplicitWait {
 
         WebDriver driver = new ChromeDriver();
         WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(10)); // explicit wait declaration
-
         driver.get("https://the-internet.herokuapp.com/javascript_alerts");
         driver.manage().window().maximize();
-
         driver.findElement(By.xpath("//button[normalize-space()='Click for JS Alert']")).click();
         Thread.sleep(5000);
-
         Alert myalert = mywait.until(ExpectedConditions.alertIsPresent()); // capture alert
-
         System.out.println(myalert.getText());
         myalert.accept();
     }
---
+++ src/test/java/day30/HandleFrames.java
@@ -0,0 +1,37 @@
+package day30;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class HandleFrames {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://ui.vision/demo/webtest/frames/");
+        //Frame 1
+        WebElement frame1 = driver.findElement(By.xpath("//frame[@src='frame_1.html']"));
+        driver.switchTo().frame(frame1);
+        driver.findElement(By.xpath("//input[@name='mytext1']")).sendKeys("welcome to ui vision");
+
+        driver.switchTo().defaultContent();
+        //Frame 2
+        WebElement frame2 = driver.findElement(By.xpath("//frame[@src='frame_2.html']"));
+        driver.switchTo().frame(frame2);
+        driver.findElement(By.xpath("//input[@name='mytext2']")).sendKeys("welcome to ui vision frame 2");
+        driver.switchTo().defaultContent();
+
+        //Frame 3
+        WebElement frame3 = driver.findElement(By.xpath("//frame[@src='frame_3.html']"));
+        driver.switchTo().frame(frame3);
+        driver.findElement(By.xpath("//input[@name='mytext3']")).sendKeys("welcome to ui vision frame 3");
+
+        //inner iframe
+        driver.switchTo().frame(0);
+        driver.findElement(By.xpath("//div[@id='i9']//div[@class='AB7Lab Id5V1']")).click();
+    }
+}
