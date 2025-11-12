9b1c65d41adc8f47f294b7d7b946a77d3b3563db
aimeerim1 <aiimuratbekovaa@gmail.com>
2025-11-12 20:30:40 +0300

    DDT reading data

---
+++ src/test/java/day40/ReadingData.java
@@ -0,0 +1,33 @@
+package day40;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadingData {
+    public static void main(String[] args) throws IOException {
+        FileInputStream file = new FileInputStream((System.getProperty("user.dir")+"/testdata/Data.xlsx"));
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheet("Лист1");
+        int totalRows = sheet.getLastRowNum();
+        int totalCells = sheet.getRow(0).getLastCellNum();
+        System.out.println("number of rows:"+totalRows);
+        System.out.println("number of cells:"+totalCells);
+
+        for(int r =0 ; r<=totalRows;r++){
+            XSSFRow currentRow =sheet.getRow(r);
+            for(int c =0 ;c<totalCells;c++){
+                XSSFCell cell =currentRow.getCell(c);
+                System.out.print(cell.toString()+"\t");
+            }
+            System.out.println();
+        }
+        workbook.close();
+        file.close();
+    }
+
+}
